cedaeb07a4d23751f81bcd431d66340c92dcf9f0
dhafin-k <dhafinekresna@gmail.com>
2025-02-10 08:33:20 +0700

    operator 2

---
+++ module-5 operator/logika.js
@@ -0,0 +1,27 @@
+
+    // Operator logika
+
+// And && operator
+let a = true
+let b = true
+let hasil = a && b
+    //butuh dua nilai yang sama untuk output true
+console.log(hasil) //maka output nya true
+                //hasilnya akan true jika dua nilai nya sama(true)
+                //jika satu saja beda maka hasil akan false
+
+// Or || operator
+let c = true
+let d = false
+let will = c || d
+    //butuh satu nilai true untuk output true
+console.log(will) // maka outputnya akan true 
+                //or akan mengembaikan true walau satu nilai nya berbedah
+                //selagi ada nilai true dan ada false maka hasil nya akan true
+
+// Not ! operator
+let e = true
+let notresult = !e
+    //karena not ! akan membalikan nilai tersebut 
+console.log(notresult) //maka output nya false
+                //jika ia true maka hasil false,jika ia false maka hasil true
---
+++ module-5 operator/ternary.js
@@ -0,0 +1,14 @@
+
+    //Operator Ternary
+
+//Condition ? expressionIfTrue : expressionIfFalse
+let age = 18
+let canVote = (age>=18 ? "yes" : "no")
+    // apakah umur user lebih dari 18?
+console.log(canVote) //maka output nya yes
+                //expression ini di bagi dua jikatrue dan jikafalse
+                //tanda (?) menandakan untuk sebuah return hasil 
+            //kedua tanda kutip di antara (:) adalah pemisah
+        //jika hasil sesuai dengan fungsi maka akan me return di bagian kiri 
+        //jika tidak maka return bagian kanan, kiri adalah true | kanan adalah false
+        //sama saja seperti kiri jika iya kanan jika tidak
